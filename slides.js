e68926df87edcfd2a6e261de633dce87bb3c1781
POLICE-GOV-IL <144675716+police-gov-il@users.noreply.github.com>
2024-01-15 19:37:24 +0200

    1

--- slides.js
+++ slides.js
@@ -13,5 +13,5 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex - 1].style.display = "block";
 }
